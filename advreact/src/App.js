8f1453a8c610bae7366621edf068bbcfa6d21183
Mohammed Afzal <mohammedafzal94@gmail.com>
2020-02-05 13:41:01 +0530

    Added Portal

--- advreact/src/App.js
+++ advreact/src/App.js
@@ -1,29 +1,21 @@
-import React from 'react';
+import React ,{Fragment} from 'react';
 import logo from './logo.svg';
 import './App.css';
 // import Toggle from './Toggle'
 import Toggle from './ToggleRPC'
+import Portal from './Portal'
 function App() {
   return (
     <div className="App">
-   {/* <Toggle render={({on,toggle}) => (
-     <div>
-       {on && <h1>Show me</h1>}
-       <button onClick={toggle}>Show/Hide</button>
-     </div>
-   ) } /> */}
-   {/* <Toggle>
-     <h1>Hello</h1>
-   </Toggle> */}
-   <Toggle>
-     {({on,toggle}) => (
-     <div>
-       {on && <h1>Show me</h1>}
-       <button onClick={toggle}>Show/Hide</button>
-     </div>
-   )}
-   </Toggle>
-      {/* <Route path='/' render={(props)=> <Login {...props}/>} /> */}
+       <Toggle>
+         { ({on,toggle}) =>(
+           <>
+             {on && <h1>Show Me</h1>}
+             <button onClick={toggle}>Show/Hide</button>
+         <Portal>{on && <h1>Hi , Im a portal !</h1>}</Portal>
+           </>
+         )}
+       </Toggle>
     </div>
   );
 }
