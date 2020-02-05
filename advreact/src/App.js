aa3e52d02b6c4a400367329dfd66968c12a2e28c
Mohammed Afzal <mohammedafzal94@gmail.com>
2020-02-05 13:01:59 +0530

    ToggleRPC

--- advreact/src/App.js
+++ advreact/src/App.js
@@ -2,16 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 // import Toggle from './Toggle'
-import Toggle from './ToggleRenderProps'
+import Toggle from './ToggleRPC'
 function App() {
   return (
     <div className="App">
-   <Toggle render={({on,toggle}) => (
+   {/* <Toggle render={({on,toggle}) => (
      <div>
        {on && <h1>Show me</h1>}
        <button onClick={toggle}>Show/Hide</button>
      </div>
-   ) } />
+   ) } /> */}
+   {/* <Toggle>
+     <h1>Hello</h1>
+   </Toggle> */}
+   <Toggle>
+     {({on,toggle}) => (
+     <div>
+       {on && <h1>Show me</h1>}
+       <button onClick={toggle}>Show/Hide</button>
+     </div>
+   )}
+   </Toggle>
       {/* <Route path='/' render={(props)=> <Login {...props}/>} /> */}
     </div>
   );
---
+++ advreact/src/ToggleRPC.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+export default class Toggle extends Component {
+    state = {
+        on : false
+    }
+    toggle = ()=>{
+        this.setState({
+            on : !this.state.on
+        })
+    }
+
+    render() {
+        console.log(this.props )
+        const {children} = this.props
+        return (
+            <div>
+             {children({
+                 on : this.state.on,
+                 toggle : this.toggle
+                 
+             })}
+            </div>
+        )
+    }
+}
