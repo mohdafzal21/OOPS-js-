84fb4dfdafe8d33b4db10776eddf74341e33c08e
Mohammed Afzal <mohammedafzal94@gmail.com>
2020-02-05 12:52:29 +0530

    ToggleRenderProps

--- advreact/src/App.js
+++ advreact/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Toggle from './Toggle'
+// import Toggle from './Toggle'
+import Toggle from './ToggleRenderProps'
 function App() {
   return (
     <div className="App">
-      <Toggle name="afzal">
-       <h1>Hello</h1>
-      </Toggle>
-      {/* <Route path='/' component={Home}>
-      <Home/>
-      </Route> */}
+   <Toggle render={({on,toggle}) => (
+     <div>
+       {on && <h1>Show me</h1>}
+       <button onClick={toggle}>Show/Hide</button>
+     </div>
+   ) } />
+      {/* <Route path='/' render={(props)=> <Login {...props}/>} /> */}
     </div>
   );
 }
